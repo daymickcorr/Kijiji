6475fcbf03d745e91bde7cdf3baaf61d6282efe2
tedgavr <32966549+tedgavr@users.noreply.github.com>
2017-10-21 16:21:04 -0400

    test2

--- Login.php
+++ Login.php
@@ -13,10 +13,11 @@ if ($securimage->check($_POST['captcha_code']) == false) {
     echo "The security code entered was incorrect.<br /><br />";
     echo "Please go <a href='javascript:history.go(-1)'>back</a> and try again.";
     exit;
+    
 }
 }
 
-//test
+//test2
 
 
 
