8de908a5817aaf5f3bc175c8f2b60154a5dfeec5
daymickcorr <mickeycorriveau@hotmail.com>
2017-11-02 13:29:31 -0400

    made some repairs on image upload

--- PostAd.php
+++ PostAd.php
@@ -5,11 +5,33 @@ require_once 'Buisness/Images.cls.php';
 require_once 'Buisness/Ad.cls.php';
 require_once 'Buisness/Payement.cls.php';
 
+/*
 if(!isset($selectedPayement)){
     $selectedPayement = new Payement();
 }
+*/
+/*
+if(isset($_POST["pay"])){
+    $payement = new Payement();
+    $payement->setPk_pay_id($_POST["pay"]);
+    $payement = $payement->find($connectionId);
 
+    $ad = new Ad();
+    $ad->setAd_description($_POST["description"]);
+    
+    $ad->setAd_price($_POST["price"]);
+    $ad->setAd_reg_date(date("Y/m/d"));
+    $ad->setAd_exp_date(date("Y/m/d") + $payement->getPay_duration());
+    $ad->setAd_tit($_POST["title"]);
+    $ad->setFk_mem_id($_SESSION["id"]);
+    $ad->setFk_pay_id($payement->getPk_pay_id());
+    $ad->setFk_subCat_id($subCat);
+    $ad->setLanguage($_SESSION["language"]);
+    $ad->create($connectionId);
+}
+*/
 //session_start() - Démarre une nouvelle session ou reprend une session existante
+/*
 echo $_SESSION["language"];
 if (isset($_SESSION["id"])){
     echo $_SESSION["id"];
@@ -21,12 +43,14 @@ else {
 if(isset($_GET["subCategory"])){
     $subCat = $_GET["subCategory"];
 }
-
+*/
+/*
 if(isset($_POST["post"])){
     echo Payement::header();
     echo $selectedPayement;
     echo Payement::footer();
-    /*
+
+
 $ad = new Ad();
 $ad->setAd_description($_POST["description"]);
 
@@ -39,22 +63,43 @@ $ad->setFk_pay_id($_POST["pay"]);
 $ad->setFk_subCat_id($subCat);
 $ad->setLanguage($_SESSION["language"]);
 $ad->create($connectionId);
-*/
 }
+*/
 
 if(isset($_FILES["images"])){
     //echo implode(',', $_FILES["images"]);
     //Créer un dossier 'fichiers/1/'
-    $path = 'Images/'.$_SESSION["id"].'/';
-    mkdir($path , 0777, true);
+    if (!is_uploaded_file($_FILES["images"]['tmp_name'])){echo "Image Upload Failed error // " . $_FILES["images"]['error'];}
+    
+    if(isset($_SESSION["id"])){
+        $sessionId = $_SESSION["id"];
+    }
+    else{$sessionId = 0;}
+    
+    $path = 'Images/'.$sessionId.'/';
+    //echo $path;
+    
+    if(is_dir($path)){}
+    else{
+        mkdir($path , 0777, true);
+    }
+   // mkdir($path , 0777, true);
     //Créer un identifiant difficile à deviner
     $nom = md5(uniqid(rand(), true));
+    
+    $extension = pathinfo($_FILES["images"]['name'],PATHINFO_EXTENSION);
     $image = new Images();
-    $image->setImagePath($path.$nom.$_FILES["images"]["type"]);
-    $image->setFk_ad_id($fk_ad_id);
+    $image->setImagePath($path.$nom.'.'.$extension);
+    
+    $image->setFk_ad_id(3);
+    
     $image->create($connectionId);
     
-    move_uploaded_file($_FILES["images"]['tmp_name'], $nom.$_FILES["images"]["type"]);
+    //echo $path.$nom.".jpg";
+    
+    //echo $extension;
+    //echo "File uploaded ".$_FILES["images"]['tmp_name'];
+    move_uploaded_file($_FILES["images"]['tmp_name'], $path.$nom.'.'.$extension);
 }
 ?>
 
@@ -82,6 +127,10 @@ if(isset($_FILES["images"])){
 	<td><td>
 </tr>
 <tr>
+	<td>Images</td>
+	<td><input type="file" name="images"><td>
+</tr>
+<tr>
 	<td>Ad Type</td>
 	<td >
 	<?php 
@@ -105,7 +154,7 @@ if(isset($_FILES["images"])){
 						<td><?php echo $element->getPay_pictures_amount()?> Images</td>
 					</tr>
 					<tr>
-						<td><input type="button" name="pay" value="<?php echo $element->getPk_pay_id()?>"></td>
+						<td><input type="submit" name="pay" value="<?php echo $element->getPk_pay_id()?>"></td>
 					</tr>
 				</th>
 			</table>
@@ -113,10 +162,6 @@ if(isset($_FILES["images"])){
 		<?php }?>
 		</td>
 </tr>
-<tr>
-	<td>Images</td>
-	<td><input type="file" name="images"><td>
-</tr>
 </table>
-<input type="submit" name="post" value="Post Ad"/>
+<!-- <input type="submit" name="post" value="Post Ad"/>  -->
 </form>
